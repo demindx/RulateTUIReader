4c81f9bd83cba6c4471635d133e8a0fcfd79bcbc
demindx <oliver9574@gmail.com>
2026-06-05 20:12:03 +0200

    fix interfaces

--- src/core/interfaces/book.py
+++ src/core/interfaces/book.py
@@ -1,15 +1,15 @@
 from abc import ABC, abstractmethod
 
-from src.models.book import Book
-from src.models.chapter import Chapter
+from src.models.book import BookModel
+from src.models.chapter import ChapterModel
 
 
 class BookRepoInterface(ABC):
     @abstractmethod
-    async def get_book(self, id: int) -> Book: ...
+    async def get_book(self, id: int) -> BookModel: ...
 
     @abstractmethod
-    async def get_chapters(self, id: int) -> list[Chapter]: ...
+    async def get_chapters(self, id: int) -> list[ChapterModel]: ...
 
     @abstractmethod
-    async def get_chapter(self, book_id: int, chapter_id: int) -> Chapter: ...
+    async def get_chapter(self, book_id: int, chapter_id: int) -> ChapterModel: ...
--- src/core/interfaces/user.py
+++ src/core/interfaces/user.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 
-from src.models.user import User
+from src.models.user import UserModel
 
 
 class UserRepoInterface(ABC):
     @abstractmethod
-    async def login(self, login: str, password: str) -> User: ...
+    async def login(self, login: str, password: str) -> UserModel: ...
 
     @abstractmethod
-    async def get_me(self) -> User: ...
+    async def get_me(self) -> UserModel: ...
