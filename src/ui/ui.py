cd19f617b9b42c41083819870aea6f6c6412bec5
demindx <oliver9574@gmail.com>
2026-06-09 15:52:57 +0200

    chapter screen push

--- src/ui/ui.py
+++ src/ui/ui.py
@@ -1,9 +1,13 @@
 from textual.app import App
+from textual import log, on
 
+from src.models.bookmark import BookmarkModel
 from src.services.app import AppService
 
 from src.ui.screens.bookmarks import BookmarksScreen
+from src.ui.screens.chapter import ChapterScreen
 from src.ui.screens.login import LoginScreen
+from src.ui.widgets.bookmark import Bookmark
 
 
 class UI(App):
@@ -23,3 +27,11 @@ class UI(App):
 
         if not self._service.keyring.get_token():
             self.push_screen(LoginScreen())
+
+    @on(Bookmark.Selected)
+    async def open_chapter(self, event: Bookmark.Selected) -> None:
+        bookmark = event.bookmark
+
+        self.push_screen(
+            ChapterScreen(book_id=bookmark.book.id, chapter_id=bookmark.last_readed)
+        )
