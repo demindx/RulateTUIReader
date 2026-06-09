911d0ae9c286b02deb8bbba83cd6927c0fce3a9e
demindx <oliver9574@gmail.com>
2026-06-09 15:52:06 +0200

    image cache

--- src/services/image.py
+++ src/services/image.py
@@ -1,20 +1,66 @@
+from time import time
 import io
 import aiohttp
 from PIL import Image, ImageDraw
 from pydantic import HttpUrl
 
 
+_MAX_CACHE_SIZE: int = 100
+
+
 class ImageService:
     def __init__(self, session: aiohttp.ClientSession) -> None:
         self._session: aiohttp.ClientSession = session
+        self._cache: dict[str, tuple[Image.Image, float]] = {}
 
-    async def get_image(self, url: HttpUrl) -> Image.Image:
+    def _get_cache_size(self) -> float:
+        return (
+            sum([len(img[0].tobytes()) for img in self._cache.values()]) / 1024
+        ) / 1024
+
+    def _get_from_cache(self, url: HttpUrl) -> Image.Image | None:
+        img = self._cache.get(str(url))
+
+        if not img:
+            return None
+
+        return img[0]
+
+    def _add_to_cache(self, img: Image.Image, url: HttpUrl) -> None:
+        size = self._get_cache_size()
+
+        while size >= _MAX_CACHE_SIZE:
+            oldest = min(self._cache.keys(), key=lambda k: self._cache[k][1])
+            size -= len(self._cache[oldest][0].tobytes())
+
+            del self._cache[oldest]
+
+        self._cache[str(url)] = (img, time())
+
+    async def get_image(self, url: HttpUrl, size: tuple[int, int]) -> Image.Image:
+        img = self._get_from_cache(url)
+
+        if img:
+            return img
+
         async with self._session.get(str(url)) as response:
             buff = io.BytesIO(await response.read())
 
-            return Image.open(buff)
+            img = Image.open(buff)
 
-    async def get_rounded_image(self, url: HttpUrl) -> Image.Image:
+            img.thumbnail(size)
+
+            self._add_to_cache(img, url)
+
+            return img
+
+    async def get_rounded_image(
+        self, url: HttpUrl, size: tuple[int, int]
+    ) -> Image.Image:
+        img = self._get_from_cache(url)
+        if img:
+            return img
+
         async with self._session.get(str(url)) as response:
             buff = io.BytesIO(await response.read())
 
@@ -36,5 +82,9 @@ class ImageService:
             draw.rounded_rectangle((0, 0, min_side, min_side), radius=radius, fill=255)
 
             img_square.putalpha(mask)
+
+            img_square.thumbnail(size)
+
+            self._add_to_cache(img_square, url)
 
             return img_square
